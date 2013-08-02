ff9058d624f9f011e8476ea0841bf1104890d4e8
jarkor@quick.cz <jarkor@quick.cz>
2013-08-02 07:56:59 +0200

    Změna textu

--- FirstApp/src/com/example/firstapp/MainActivity.java
+++ FirstApp/src/com/example/firstapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.firstapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        TextView tv =  (TextView) findViewById(R.id.text1);
+        tv.setText("Hokus pokus");
     }
 
 
